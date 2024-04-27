bd3262f526e93644710e167843e6b5847f80f99f
1024小神 <15670339118@163.com>
2024-04-27 21:36:53 +0800

    add lottery

--- src/douyin/live.rs
+++ src/douyin/live.rs
@@ -1,9 +1,10 @@
 use crate::douyin::apis::DouYinReq;
 
+// 中间控制层
 pub async fn lottery_main() {
     println!("获取抖音直播间福袋的主逻辑");
     // 先创建一个抖音直播间请求结构体：包含请求对象和一些请求api
-    let live_url = String::from("https://live.douyin.com/972176515698");
+    let live_url = String::from("https://live.douyin.com/926054037870");
     let mut live_req = DouYinReq::new(live_url);
     // 执行搜索请求的api
     live_req.get_room_id().await.unwrap();
